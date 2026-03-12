68372a922340ee90aa4957929e77a86de3a1068a
shinchuanchenne <chenneshinchuan@gmail.com>
2026-03-12 13:17:36 +0800

    chore(schemas): add user.py schemas.

---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field, EmailStr
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    username: str = Field(min_length=1, max_length=64)
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=256)
+
+class UserRead(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    created_at: datetime
+
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=64)
+    email: EmailStr | None = None
+    password: str | None = Field(default=None, min_length=8, max_length=256)
