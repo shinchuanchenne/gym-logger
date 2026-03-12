bae89f897299613b690d6fe694ef566f68c7a3d8
shinchuanchenne <chenneshinchuan@gmail.com>
2026-03-12 19:05:30 +0800

    chore(core): update config.py and .env with secret key, access token expire minutes and algorithm

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,6 @@
 from pwdlib import PasswordHash
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from datetime import datetime, timedelta, timezone
 
 password_hash = PasswordHash.recommended()
 
@@ -7,3 +9,4 @@ def hash_password(plain_password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
+
