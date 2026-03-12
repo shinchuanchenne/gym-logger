13b5eb106536da8cd265c3acefe1985e7fc5d8d3
shinchuanchenne <chenneshinchuan@gmail.com>
2026-03-12 19:14:39 +0800

    feat(core): create create_access_token and import jwt

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,9 @@
 from pwdlib import PasswordHash
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from datetime import datetime, timedelta, timezone
+import jwt
+from jwt.exceptions import InvalidTokenError
+from app.core.config import settings
 
 password_hash = PasswordHash.recommended()
 
@@ -10,3 +13,18 @@ def hash_password(plain_password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
 
+# JWT
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+    return  encoded_jwt
